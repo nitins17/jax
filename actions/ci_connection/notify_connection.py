bb3e9a033e2ff44fd83565682854672fc38163ab
Nitin Srinivasan <srnitin@google.com>
2024-08-27 06:07:05 +0000

    fix env path and enable rbe in workflow build

--- actions/ci_connection/notify_connection.py
+++ actions/ci_connection/notify_connection.py
@@ -53,8 +53,9 @@ if __name__ == "__main__":
     # Set the hardcoded envs for testing purposes
     # TODO: sync env vars
     sub_env = os.environ.copy()
-    sub_env["ENV_FILE"] = "ci/envs/build_artifacts/jaxlib/linux_x86/py312"
+    sub_env["ENV_FILE"] = "ci/envs/build_artifacts/jaxlib"
     sub_env["JAXCI_USE_DOCKER"] = "0"
+    sub_env["JAXCI_USE_RBE"] = "1"
 
     # Enter interactive bash session
     subprocess.run(["/bin/bash", "-i"], env=sub_env)
